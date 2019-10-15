97d0f0087250df17876527e0b1c56384ab1bb99b
manuk <mahhov1@gmail.com>
2019-10-15 16:56:03 -0700

    wip video frame with play function

--- src/videoFrame/VideoFrame.js
+++ src/videoFrame/VideoFrame.js
@@ -1,8 +1,13 @@
 const {XElement, importUtil} = require('xx-element');
 const {template, name} = importUtil(__filename);
+const storage = require('../service/storage');
 
 customElements.define(name, class extends XElement {
 	static get htmlTemplate() {
 		return template;
+	}
+
+	async play(name) {
+		this.$('video').src = await storage.videoPath(name);
 	}
 });
