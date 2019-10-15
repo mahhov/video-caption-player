6f9f34c8a4612be6662e713627af89da4b4d52fa
manuk <mahhov1@gmail.com>
2019-10-15 15:43:36 -0700

    dir frame mostly done with clickable dirs and filter input. styling needed

--- src/dirFrame/dirFrame.js
+++ src/dirFrame/dirFrame.js
@@ -1,8 +1,35 @@
 const {XElement, importUtil} = require('xx-element');
 const {template, name} = importUtil(__filename);
+const storage = require('../service/Storage');
 
 customElements.define(name, class extends XElement {
 	static get htmlTemplate() {
 		return template;
+	}
+
+	connectedCallback() {
+		this.refreshList_();
+		this.$('#search-input').addEventListener('input', () => this.filterList_())
+	}
+
+	async refreshList_() {
+		this.clearChildren('#list');
+		(await storage.videoList).map(name => {
+			let line = document.createElement('div');
+			line.textContent = name;
+			line.addEventListener('click', () =>
+				this.dispatchEvent(new CustomEvent('select-video', {detail: name})));
+			this.$('#list').appendChild(line);
+		})
+	}
+
+	filterList_() {
+		const charFilterRe = /[^a-z\d]/g;
+		let filterWords = this.$('#search-input').value.toLowerCase().split(charFilterRe);
+
+		this.$$('#list > div').forEach(line => {
+			let lineText = line.textContent.toLowerCase().replace(charFilterRe, '');
+			line.classList.toggle('hidden', !filterWords.every(word => lineText.includes(word)))
+		});
 	}
 });
